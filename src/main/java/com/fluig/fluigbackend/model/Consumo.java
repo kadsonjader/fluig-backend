d50fa127e4a78ff89e2f1dc4b87c92e8f51af684
kadsonjader <kadsonj@hotmail.com>
2021-10-02 10:12:16 -0400

    ajutes de migraion e criação de modelos

---
+++ src/main/java/com/fluig/fluigbackend/model/Consumo.java
@@ -0,0 +1,26 @@
+package com.fluig.fluigbackend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "EFLUIGCONSUMO")
+@Getter @Setter
+public class Consumo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cdConsumo;
+
+    private Integer cdVeiculo;
+    private Double nuPrecoGasolina;
+    private Double nuKmCidade;
+    private Double nuKmRodovia;
+
+    @ManyToOne
+    @JoinColumn(name = "cdVeiculo", updatable = false, insertable = false, nullable = true)
+    private Veiculo veiculo;
+}
--- src/main/resources/db/migration/V001_Modelagem_inicial_ddl.sql
+++ src/main/resources/db/migration/V001_Modelagem_inicial_ddl.sql
@@ -10,8 +10,8 @@ CREATE TABLE EFLUIGVEICULOS
     nmmarca          VARCHAR(255),
     nmmodelo         VARCHAR(255),
     dtfabricacao     TIMESTAMP,
-    nuconsumocidade  VARCHAR(1),
-    nuconsumorodovia VARCHAR(10),
+    nuconsumocidade  NUMERIC(13,2),
+    nuconsumorodovia NUMERIC(13,2),
     CONSTRAINT pkveiculo PRIMARY KEY (cdveiculo)
 );
 
@@ -20,8 +20,8 @@ CREATE TABLE EFLUIGCONSUMO
     cdconsumo       NUMERIC(10),
     cdveiculo       NUMERIC(10),
     nuprecogasolina NUMERIC(13, 2) NOT NULL,
-    nukmcidade      NUMERIC(10)    NOT NULL,
-    nukmrodovia     NUMERIC(10)    NOT NULL,
+    nukmcidade      NUMERIC(13,2)    NOT NULL,
+    nukmrodovia     NUMERIC(13,2)    NOT NULL,
     CONSTRAINT pkconsumo PRIMARY KEY (cdconsumo)
 );
 
